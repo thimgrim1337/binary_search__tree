93d26d5593a1c148a01126948d276b0ec00aec37
thimgrim1337 <d.dygner@hotmail.com>
2023-06-08 10:47:06 +0200

    Implement delete() to class Tree

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.leftNode = null;
-    this.rightNode = null;
+    this.leftNode = this.rightNode = null;
   }
 
   setLeftNode = (node) => (this.leftNode = node);
@@ -38,11 +37,38 @@ class Tree {
       root.setRightNode(this.insert(value, root.rightNode));
     return root;
   };
+
+  delete = (value, root = this.root) => {
+    if (root === null) return root;
+
+    if (value < root.value) root.setLeftNode(this.delete(value, root.leftNode));
+    if (value > root.value)
+      root.setRightNode(this.delete(value, root.rightNode));
+    else {
+      if (root.leftNode === null) return root.rightNode;
+      if (root.rightNode === null) return root.leftNode;
+
+      root.value = this.minValue(root.rightNode);
+
+      root.rightNode = this.delete(root.value, root.rightNode);
+    }
+    return root;
+  };
+
+  minValue = (root) => {
+    let min = root.value;
+    while (root.leftNode !== null) {
+      min = root.leftNode.value;
+      root = root.leftNode;
+    }
+    return min;
+  };
 }
 
 const arr = [1, 7, 7, 4, 23, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(arr);
 tree.insert(1000);
+tree.delete(6345);
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node === null) {
